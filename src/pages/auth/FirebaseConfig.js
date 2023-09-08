b4635f11f6add0783bc431b3a1505ad9b7f6e0ee
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2023-09-08 23:43:02 +0200

    update src/pages/auth/FirebaseConfig.js

--- src/pages/auth/FirebaseConfig.js
+++ src/pages/auth/FirebaseConfig.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase 
-const app = firebase.initializeApp (firebaseConfig);
+const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 
 const auth = app.auth();
