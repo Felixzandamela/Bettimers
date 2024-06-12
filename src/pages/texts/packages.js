e513e19307c7857b62b01f4d334160dff317b7a7
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2024-06-12 16:34:52 +0200

    update src/pages/texts/packages.js

--- src/pages/texts/packages.js
+++ src/pages/texts/packages.js
@@ -1,19 +1,19 @@
 export const packages = [
   {
     "name": "monthly",
-    "amount": 1264.99,
+    "amount":499.99,
     "prefix":["Mês", "Month"],
     "discount":0
   },
   {
     "name": "quarterly",
-    "amount": 3035.99,
+    "amount": 1299.99,
     "prefix":["Trimestre","Quarter"],
     "discount":20
   },
     {
     "name": "yearly",
-    "amount": 1264.99,
+    "amount": 3499.99,
     "prefix":["Ano","Year"],
     "discount":46
   }
